fdde5b4888f1eb057a5fe0e1635140a62239aa6d
Mohamed Tarek <moka76615@gmail.com>
2023-04-24 00:46:31 +0200

    product & customer test

--- src/main/java/com/example/pos_system/DAO/ProductDAOTest.java
+++ src/main/java/com/example/pos_system/DAO/ProductDAOTest.java
@@ -12,10 +12,17 @@ class ProductDAOTest {
     void addNewProduct() {
         CashierDAOImp obj=new CashierDAOImp();
         ProductDAOImp obj1=new ProductDAOImp();
+
+
         Product p=new Product(1,"Laptop",900,1000,90,Date.valueOf("2026-2-2"),Date.valueOf("2023-1-1"),1,"China","Laptop");
-        //obj1.addNewProduct(p);
+        obj1.addNewProduct(p);
+
+        Product p2=new Product(2,"Laptop",900,1000,90,Date.valueOf("2026-2-2"),Date.valueOf("2023-1-1"),10,"China","Laptop");
+        obj1.addNewProduct(p2);
+
         assertAll(
-                () -> assertEquals(true,obj.isExist(1))
+                () -> assertEquals(true,obj.isExist(1)),
+                () -> assertEquals(true,obj.isExist(2))
         );
     }
     @org.junit.jupiter.api.Test
@@ -24,14 +31,17 @@ class ProductDAOTest {
         ProductDAOImp obj1=new ProductDAOImp();
         Product p=new Product(1,"mobile",900,1000,90,Date.valueOf("2026-2-2"),Date.valueOf("2023-1-1"),1,"China","Laptop");
         obj1.ubdateProduct(p);
+        assertAll(
+                () -> assertEquals(p.toString(),obj.getProduct(1).toString())
+        );
+
+
+
     }
 
     @org.junit.jupiter.api.Test
     void allSales() {
         ProductDAOImp obj1=new ProductDAOImp();
-        Product p;
-        p=new Product(2,"Laptop",900,1000,90,Date.valueOf("2026-2-2"),Date.valueOf("2023-1-1"),10,"China","Laptop");
-        //obj1.addNewProduct(p);
         assertAll(
                 () -> assertEquals(11,obj1.allSales())
         );
@@ -42,10 +52,10 @@ class ProductDAOTest {
     void deleteProduct() {
         CashierDAOImp obj=new CashierDAOImp();
         ProductDAOImp obj1=new ProductDAOImp();
-        Product p=new Product(1,"Laptop",900,1000,90,Date.valueOf("2026-2-2"),Date.valueOf("2023-1-1"),1,"China","Laptop");
+        Product p=new Product(2,"Laptop",900,1000,90,Date.valueOf("2026-2-2"),Date.valueOf("2023-1-1"),1,"China","Laptop");
         obj1.deleteProduct(p);
         assertAll(
-                () -> assertEquals(obj.isExist(1),false)
+                () -> assertEquals(obj.isExist(2),false)
         );
     }
 
